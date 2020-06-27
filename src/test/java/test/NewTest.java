4df96c2c577f23fe947ecc9e3093d84b15b3efcf
Sreedevi <Sreedevi@DESKTOP-F9MKETK>
2020-06-27 15:52:30 +0530

    testtt

--- src/test/java/test/NewTest.java
+++ src/test/java/test/NewTest.java
@@ -2,6 +2,7 @@ package test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -11,9 +12,15 @@ public class NewTest {
 	public WebDriver driver;
 	
   @Test
-  public void openMyBlog() {
-	  System.setProperty("webdriver.chrome.driver","chromedriverfdg.exe");
+  public void openMyBlog() throws InterruptedException {
+	  System.setProperty("webdriver.chrome.driver","chromedriver.exe");
 
 	   driver = new ChromeDriver();
 	driver.get("https://www.GOOGLE.com/");
+	Thread.sleep(5000);
+	String actualTitle = driver.getTitle();
+	driver.manage().window().maximize();
+	String expectedTitle = "Avinash Mishra Blogger: Learn Selenium WebDriver, Automation Framework, QA, SEO & Digital Marketing | Inviul Blog | Inviul";
+	Assert.assertEquals("Condition true", actualTitle, expectedTitle);
+	
   }}
